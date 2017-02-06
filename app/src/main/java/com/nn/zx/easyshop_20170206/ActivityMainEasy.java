bcf117a91c717473f05cba7b42070716a85e49c1
赵燕音 <513377972@qq.com>
2017-02-06 15:28:59 +0800

    first commit

---
+++ app/src/main/java/com/nn/zx/easyshop_20170206/ActivityMainEasy.java
@@ -0,0 +1,13 @@
+package com.nn.zx.easyshop_20170206;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMainEasy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_easy);
+    }
+}
